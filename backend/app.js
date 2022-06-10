7508cfba9bee4fa1c116ba2f09ff6ee254cbee8c
Laura Flament <laura.flament6@gmail.com>
2022-06-10 16:36:50 +0200

    Ajout des modèles, parcours et contrôleurs  utilisateurs et sauces

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Laura:Hzkum56@cluster0.mvs6y.mongodb.net/?retryWrites=true&w=majority',
 { useNewUrlParser: true,
@@ -12,14 +15,27 @@ mongoose.connect('mongodb+srv://Laura:Hzkum56@cluster0.mvs6y.mongodb.net/?retryW
 
 app.use(express.json());
 
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
+app.use(cors());
+/*
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+*/
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app; 
