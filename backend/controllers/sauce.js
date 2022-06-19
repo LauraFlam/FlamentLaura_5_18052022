017cf8618333f02575b394179dc4d1e3f5dc2b5e
Laura Flament <laura.flament6@gmail.com>
2022-06-19 18:36:59 +0200

    Modification controllers sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,13 +1,16 @@
 const Sauce = require('../models/sauce');
 const fs = require('fs');
+const { stringify } = require('querystring');
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
+    console.log(sauceObject);
+    /*delete sauceObject._id; */
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
+    console.log(sauce);
     sauce.save()
         .then(() => res.status(201).json({ message: 'Sauce enregistrée !' }))
         .catch(error => res.status(400).json({ error }));
