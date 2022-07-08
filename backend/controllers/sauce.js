0fa38b1c08152026cd49e66ab24b5b79ea82c313
Laura Flament <laura.flament6@gmail.com>
2022-07-08 13:20:50 +0200

    Mise en place des likes et dislikes

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -30,27 +30,75 @@ exports.modifySauce = (req, res, next) => {
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
+        .then(sauce => { 
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
-                    .catch(error => res.status(400).json({ error }));
-            });
-        })
-        .catch(error => res.status(500).json({ error }));
-    Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-            if (!sauce) {
-                return res.status(404).json({ error: new Error('Sauce non trouvée') });
-            }
-            if (sauce.userId !== req.auth.userId) {
-                return res.status(401).json({ error: new Error('Requête non authorisée !') });
-            }
             Sauce.deleteOne({ _id: req.params.id })
                 .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
                 .catch(error => res.status(400).json({ error }));
-        });
+            });
+    })
+        .catch(error => res.status(500).json({ error }));
+};
+
+exports.likeDislikeSauce = (req, res, next) => {
+    // On récupère la sauce
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+
+
+            switch (req.body.like) {
+                //Si l'utilisateur dislike : 
+                case -1:
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $inc: { dislikes: 1 },
+                        $push: { usersDisliked: req.body.userId },
+                        _id: req.params.id
+                    })
+                        .then(() => res.status(201).json({ message: 'Dislike pris en compte !' }))
+                        .catch(error => res.status(400).json({ error }))
+                    break;
+
+                case 0:
+                    //Si la sauce est déjà liké :
+                    if (sauce.usersLiked.find(user => user === req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                            $inc: { likes: -1 },
+                            $pull: { usersLiked: req.body.userId },
+                            _id: req.params.id
+                        })
+                            .then(() => res.status(201).json({ message: ' Avis mis à jour !' }))
+                            .catch(error => res.status(400).json({ error }))
+                    }
+
+                    //Si la sauce est déjà disliké :
+                    if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+                        Sauce.updateOne({ _id: req.params.id }, {
+                            $inc: { dislikes: -1 },
+                            $pull: { usersDisliked: req.body.userId },
+                            _id: req.params.id
+                        })
+                            .then(() => res.status(201).json({ message: ' Avis mis à jour !' }))
+                            .catch(error => res.status(400).json({ error }));
+                    }
+                    break;
+
+                //Si l'utilisateur like la sauce, +1 :
+                case 1:
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $inc: { likes: 1 },
+                        $push: { usersLiked: req.body.userId },
+                        _id: req.params.id
+                    })
+                        .then(() => res.status(201).json({ message: 'Like pris en compte !' }))
+                        .catch(error => res.status(400).json({ error }));
+                    break;
+                default:
+                    return res.status(500).json({ error });
+            }
+        })
+        .catch(error => res.status(500).json({ error }))
+    console.log(req.body);
 };
 
 exports.getOneSauce = (req, res, next) => {
